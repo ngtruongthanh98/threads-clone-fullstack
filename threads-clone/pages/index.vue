471ee846d320ecaf22e009181c309c65ad4342b0
Luca <ngtruongthanh98@gmail.com>
2023-08-04 21:20:54 +0700

    feat: page index UI

--- threads-clone/pages/index.vue
+++ threads-clone/pages/index.vue
@@ -1,9 +1,37 @@
 <template>
   <MainLayout>
-    INDEX
+    <div id="IndexPage" class="w-full overflow-auto">
+      <div class="mx-auto max-w-[500px] overflow-hidden">
+        <div id="Posts" class="px-4 max-w-[600px] mx-auto">
+
+          <div v-if="isPosts" v-for="post in posts" :key="post">
+            <Post :post="post" @isDeleted="posts = []"/>
+          </div>
+        </div>
+      </div>
+    </div>
   </MainLayout>
 </template>
 
 <script setup>
 import MainLayout from '~/layouts/MainLayout.vue'
+
+import { useUserStore } from '~/stores/user'
+const userStore = useUserStore()
+// const user = useSupabaseUser()
+
+let posts = ref([])
+let isPosts = ref(false)
+let isLoading = ref(false)
+
+onBeforeMount(() => {
+  posts.value = [
+    {
+      name: 'John Doe',
+      image: 'https://placehold.co/100',
+      text: 'This is the title',
+      picture: 'https://placehold.co/100',
+    }
+  ]
+})
 </script>
